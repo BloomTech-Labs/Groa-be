778bb0c4da8bed0d19492a72a742acb7382d2116
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-04-20 22:09:21 -0400

    moved helper into db folder

--- database/helpers/prepTestDB.js
+++ database/helpers/prepTestDB.js
@@ -1,17 +1,16 @@
-const db = require("../database/dbConfig.js");
+const db = require("../dbConfig.js");
 
 module.exports = {
   prepTestingDB,
-  truncateTable
+  truncateTable,
 };
 /**
  * Preps the testing DB but truncating a choosen table, and running seed data
  * @param {string} table_name - matches the table name you'd like to truncate
  */
 async function prepTestingDB(table_name) {
-  await db.raw(`TRUNCATE ${table_name} RESTART IDENTITY CASCADE`)
+  await db.raw(`TRUNCATE ${table_name} RESTART IDENTITY CASCADE`);
 }
 async function truncateTable(table_name) {
   await db.raw(`TRUNCATE ${table_name} RESTART IDENTITY CASCADE`);
 }
-
