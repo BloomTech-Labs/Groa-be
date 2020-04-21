4b795f884ac34f145d1438bc31a53296f74af5f9
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-04-20 21:50:09 -0400

    added new columns to movies db

--- movies/movies-model.js
+++ movies/movies-model.js
@@ -1,26 +1,29 @@
 const db = require("../database/dbConfig.js");
 
 module.exports = {
-    getAllMovies
-}
+  getAllMovies,
+};
 
 function getAllMovies(user_id) {
-    return db("movies as m")
+  return db("movies as m")
     .where("title_type", "movie")
     .select(
-        "m.movie_id as id",
-        "m.primary_title as name",
-        "m.start_year as year",
-        "m.runtime_minutes",
-        "m.genres",
-        "m.poster_url",
-        "m.average_rating",
-        "m.num_votes",
-        "m.release_date",
-        "m.original_language",
+      "m.movie_id as id",
+      "m.primary_title as name",
+      "m.start_year",
+      "m.end_year",
+      "m.runtime_minutes",
+      "m.genres",
+      "m.poster_url",
+      "m.average_rating",
+      "m.num_votes",
+      "m.release_date",
+      "m.original_language",
+      "m.description",
+      "m.trailer_url"
     )
     .whereNotNull("m.average_rating")
     .whereNotNull("m.poster_url")
     .whereNotNull("m.num_votes")
-    .orderBy("m.num_votes", "desc")
+    .orderBy("m.num_votes", "desc");
 }
