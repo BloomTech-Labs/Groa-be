d06c37df39f8f951031918565f5429757bc0715c
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-04-20 21:53:06 -0400

    fixed typo on directory path

--- users_movie_data/groa-user-router.js
+++ users_movie_data/groa-user-router.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 const {
   addRating,
   getRatings
-} = require("./user_table_model/ratings.js");
+} = require("./user_table_models/ratings.js");
 
 // middleware
 const validateRatingBody = require("./middleware/validateRatingBody.js");
