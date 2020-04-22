7026010a9c97b061c99322df308a92a4fc1b02aa
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-04-22 12:13:35 -0400

    changed model to show all information for movie

--- movies/movies-model.js
+++ movies/movies-model.js
@@ -7,21 +7,7 @@ module.exports = {
 function getAllMovies(user_id) {
   return db("movies as m")
     .where("title_type", "movie")
-    .select(
-      "m.movie_id as id",
-      "m.primary_title as name",
-      "m.start_year",
-      "m.end_year",
-      "m.runtime_minutes",
-      "m.genres",
-      "m.poster_url",
-      "m.average_rating",
-      "m.num_votes",
-      "m.release_date",
-      "m.original_language",
-      "m.description",
-      "m.trailer_url"
-    )
+    .select("*")
     .whereNotNull("m.average_rating")
     .whereNotNull("m.poster_url")
     .whereNotNull("m.num_votes")
