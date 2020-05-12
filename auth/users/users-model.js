171490c4bb15799a8952222177bf98238309e378
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-05-12 11:01:30 -0400

    fixed getUserData

--- auth/users/users-model.js
+++ auth/users/users-model.js
@@ -6,82 +6,77 @@ module.exports = {
   getUserById,
   findUsers,
   getUserDataByOktaId,
-  getUserData
+  getUserData,
 };
 
 function add(user) {
   return db("users")
     .insert(user, "user_id")
-    .then(ids => {
+    .then((ids) => {
       const [user_id] = ids;
-      return db("users")
-        .where({ user_id })
-        .first();
-    }).catch(error => {
-      console.log(error)
+      return db("users").where({ user_id }).first();
+    })
+    .catch((error) => {
+      console.log(error);
     });
 }
 
-function findBy(user_name) {
-  return db("users")
-    .where("user_name", user_name)
-    .first();
+function findBy(email) {
+  return db("users").where("email", email).first();
 }
 
 function getUserById(user_id) {
-  return db("users")
-          .where({ user_id })
-          .select('*')
-          .first();
+  return db("users").where({ user_id }).select("*").first();
 }
 
 // function findByOktaId(okta_id) {
 //   return db("users")
 //           .where("okta_id", okta_id)
 //           .first();
-// } 
+// }
 
 async function getUserDataByOktaId(okta_id) {
-  let user = await getUserById(okta_id)
-  .select("*")
+  let user = await getUserById(okta_id).select("*");
   await db("user_ratings")
-  .where("user_id", user.user_id)
-  .then(ratings => {
-    user = {
-      ...user, ratings
-    }
-  })
+    .where("user_id", user.user_id)
+    .then((ratings) => {
+      user = {
+        ...user,
+        ratings,
+      };
+    });
   await db("user_watchlist")
-  .where("user_id", user.user_id)
-  .then(watchlist => {
-    user = {
-      ...user, watchlist
-    }
-  })
+    .where("user_id", user.user_id)
+    .then((watchlist) => {
+      user = {
+        ...user,
+        watchlist,
+      };
+    });
   return user;
-};
-
-function findUsers() {
-  return db("users")
-    .select("user_name", "user_id")
 }
 
-async function getUserData(user_name) {
-  let user = await findBy(user_name)
-  .select("*")
+function findUsers() {
+  return db("users").select("user_name", "user_id");
+}
+
+async function getUserData(email) {
+  let user = await findBy(email).select("*");
   await db("user_ratings")
-  .where("user_id", user.user_id)
-  .then(ratings => {
-    user = {
-      ...user, ratings
-    }
-  })
+    .where("user_id", user.user_id)
+    .then((ratings) => {
+      user = {
+        ...user,
+        ratings,
+      };
+    });
   await db("user_watchlist")
-  .where("user_id", user.user_id)
-  .then(watchlist => {
-    user = {
-      ...user, watchlist
-    }
-  })
+    .where("user_id", user.user_id)
+    .then((watchlist) => {
+      user = {
+        ...user,
+        watchlist,
+      };
+    });
   return user;
-};
+}
